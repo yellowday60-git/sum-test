bf5b1ad9a92c577cb47e95daede80dd4d747a6b1
yellowday60 <yellowday60@naver.com>
2022-07-03 14:36:33 +0900

    Update sum modules

--- sum.cpp
+++ sum.cpp
@@ -1,8 +1,14 @@
 // sum.cpp
 #include "sum.h"
 
+// int sum(int n) {
+
+//   return n*(n+1)/2;
+// }
+
 int sum(int n) {
-
-  return n*(n+1)/2;
+  int res = 0;
+  for (int i = 1; i <= n; ++i)
+    res += i;
+  return res;
 }
-
